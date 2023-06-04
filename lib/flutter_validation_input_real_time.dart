105c165f0a7e794e0dd14d1dbe8cc38abb8ed830
Michael Nabil <michael.nabil230@gmail.com>
2023-06-04 04:35:41 +0300

    Add export

--- lib/flutter_validation_input_real_time.dart
+++ lib/flutter_validation_input_real_time.dart
@@ -1,1 +1,7 @@
 library flutter_validation_input_real_time;
+
+export 'package:flutter_validation_input_real_time/src/rules/index.dart';
+export 'package:flutter_validation_input_real_time/src/input.dart';
+export 'package:flutter_validation_input_real_time/src/validation_text_editing_controller.dart';
+export 'package:flutter_validation_input_real_time/src/widgets/text_field_custom.dart';
+export 'package:flutter_validation_input_real_time/src/widgets/validation_form.dart';
