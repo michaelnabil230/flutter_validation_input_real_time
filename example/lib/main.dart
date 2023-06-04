dacbadc773b47c3d643a08b61dd9e55432c74408
Michael Nabil <michael.nabil230@gmail.com>
2023-06-04 04:39:43 +0300

    Add the example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,13 +11,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late ValidationTextEditingController _emailController;
+  late ValidationTextEditingController _passwordController;
+
   @override
   void initState() {
+    _emailController = ValidationTextEditingController();
+    _passwordController = ValidationTextEditingController();
     super.initState();
   }
 
   @override
   void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
@@ -28,8 +35,50 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Welcome to the real-time validator'),
         ),
-        body: const Center(
-          child: Text('Soon'),
+        body: ValidationForm(
+          onChanged: (bool isPass) {
+            print('Is pass $isPass');
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFieldCustom(
+                  name: 'email',
+                  hintText: 'Email',
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  rules: [
+                    IsRequired(),
+                    IsEmail(),
+                  ],
+                ),
+                TextFieldCustom(
+                  name: 'password',
+                  hintText: 'Password',
+                  controller: _passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  rules: [
+                    IsRequired(),
+                    RangeLength(min: 8, max: 12),
+                  ],
+                ),
+                InkWell(
+                  onTap: () {
+                    print('Login');
+                  },
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    color: Colors.amber,
+                    alignment: Alignment.center,
+                    child: const Text('Login'),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
